85a7baa25162eab6211ac8103ff2425a0827209f
wkang <wkang2015@163.com>
2023-07-06 00:32:05 +0800

    Update cmake buid settings

--- demo-cxx/protoc-gen-cxx.sh
+++ demo-cxx/protoc-gen-cxx.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
-declare -r GRPC_PATH='third-party/build/target/grpc/bin'
-declare -r PROTOC_PATH='third-party/build/target/protobuf/bin'
+declare -r GRPC_PATH='../third-party/build/target/grpc/bin'
+declare -r PROTOC_PATH='../third-party/build/target/protobuf/bin'
 
 protoc_gen_cxx() {
-  local dir="./gen"
-  mkdir -p "${dir}"
+  local dir="./gen-cxx"
+  mkdir -p "${dir}" "${dir}-grpc"
   ${PROTOC_PATH:?}/protoc \
     --cpp_out="${dir}" \
-    --grpc_out="${dir}" \
+    --grpc_out="${dir}-grpc" \
     --plugin=protoc-gen-grpc="${GRPC_PATH:?}/grpc_cpp_plugin" \
     --proto_path=../proto/googleapis \
     --proto_path=../proto/grpc-gateway \
