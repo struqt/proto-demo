1f47e6b52c28f533fd6dd5e7b8bb85a9d1132891
wkang <wkang2015@163.com>
2023-07-07 18:46:57 +0800

    [workflows] for bot

--- demo-rs/protoc-gen-rust.sh
+++ demo-rs/protoc-gen-rust.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 declare -r SELF=$(readlink -f "$0")
 declare -r SELF_DIR=${SELF%/*}
-
 echo "Run $SELF"
-
 
 #export PROTOBUF_LOCATION=$(nix-env -q protobuf --out-path --no-name)
 #export PROTOC=$PROTOBUF_LOCATION/bin/protoc
 #export PROTOC_INCLUDE=$PROTOBUF_LOCATION/include
 
-
 pushd "${SELF_DIR}"
-
-cargo build
-
+cargo build --release --bin demo-rs-server
 popd
-
