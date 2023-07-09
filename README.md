1e0d582fc7aa4c788d685c35491de374ff080200
wkang <wkang2015@163.com>
2023-07-10 00:26:51 +0800

    Use rt-multi-thread

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 [![Main Workflow](https://github.com/struqt/proto-demo/actions/workflows/main.yml/badge.svg)](https://github.com/struqt/proto-demo/actions/workflows/main.yml)
 [![Lint](https://github.com/struqt/proto-demo/actions/workflows/lint.yml/badge.svg)](https://github.com/struqt/proto-demo/actions/workflows/lint.yml)
+
+---
+
+
+## Performance References
+
+[grpc.io - Benchmarking](https://grpc.io/docs/guides/benchmarking/)
+
+[grpc.io - Performance Best Practices](https://grpc.io/docs/guides/performance)
+
+[github.com - grpc_bench](https://github.com/LesnyRumcajs/grpc_bench)
+
--- demo-rs/src/server/main.rs
+++ demo-rs/src/server/main.rs
@@ -1,11 +1,26 @@
 mod struqt;
 mod handler;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:9090".parse().unwrap();
-    let ctx = &handler::context();
-    println!("Server is listening on {}", addr);
-    handler::server(ctx).serve(addr).await?;
+type Yes<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Yes<()> {
+    let threads = std::env::var("GRPC_WORKER_THREADS")
+        .map(|v| v.parse().unwrap())
+        .unwrap_or(2);
+    println!("Running with {} threads", threads);
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(threads)
+        .enable_all()
+        .build()?
+        .block_on(serve())
+}
+
+async fn serve() -> Yes<()> {
+    let context = &handler::context();
+    let address = "127.0.0.1:9090".parse()?;
+    let router = handler::server(context);
+    println!("Server is listening on {}", address);
+    router.serve(address).await?;
+    println!("Server is shutting down");
     Ok(())
 }
